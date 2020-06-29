8859327128d1dd08eab28c97f5790f0a37f4199f
q515064362 <54224849+q515064362@users.noreply.github.com>
2020-06-29 10:29:14 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,49 @@
-11111
-2222
-33333
+# Load libraries
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot
+from pandas import read_csv
+import seaborn as sns
+from numpy import arange
+from pandas import  set_option
+from pandas.plotting import scatter_matrix
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error
+from scipy.stats import zscore
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.metrics import mean_squared_error
+
+
+%matplotlib inline
+
+import warnings
+warnings.filterwarnings('ignore')
+
